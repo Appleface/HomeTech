d2cd20cedae2be7685d545cdf1758fa0958ddcfc
appleface2050 <appleface2050@qq.com>
2019-05-05 10:46:26 +0800

    libilibi

---
+++ handle_youtube_srt/handle_youtube_srt.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+import datetime
+import time
+import os
+import sys
+
+
+def extract_subtitle(file_dir):
+    f = open(file_dir)
+    f.readlines()
+
+
+    f.close()
+
+
+
+if __name__ == '__main__':
+    file_dir = "../srts/(auto)Application Submitted to Sponsorship my Parents to Canada!!! - YouTube.srt"
